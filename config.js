64da0a2d0eb0803560b47bd3615a8f5b0ed70b0a
jargdovz <thehousedeeper@gmail.com>
2026-05-24 01:25:28 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBaDVVlgROTXKUkrpxgvg5vN-HiuaNu6Fs",
+  authDomain: "airesgm-v1.firebaseapp.com",
+  databaseURL: "https://airesgm-v1-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "airesgm-v1",
+  storageBucket: "airesgm-v1.firebasestorage.app",
+  messagingSenderId: "947262540740",
+  appId: "1:947262540740:web:e708a99a4728fd4e2d3291"
+};
